89b903b90b074e26fafbfcfc1c56fee818c0db02
libertinistic <libertinistic@gmail.com>
2016-07-26 17:54:39 -0700

    refinements to program flow

--- test/tests.js
+++ test/tests.js
@@ -26,45 +26,65 @@ describe('HTTP server api', function(done) {
     });
   });
 
-  // it('is able to get all records', function(done) {
-  //   request
-  //   .get('/')
-  //   .end((err, res) => {
-  //     if (err) return done(err);
-  //     assert.deepEqual(JSON.parse(res.text), [ { id: 1, data: 'foobarbaz' }, { id: 2, data: 'barfoobaz' } ]);
-  //     done();
-  //   });
-  // });
-  //
-  // it('is able to get a specific record', function(done) {
-  //   request
-  //   .get('/api/2')
-  //   .end((err, res) => {
-  //     if (err) return done(err);
-  //     // console.log((res));
-  //     assert.deepEqual(JSON.parse(res.text), { id: 2, data: 'barfoobaz' });
-  //     done();
-  //   });
-  // });
-  //
-  // it('is able to update a record', function(done) {
-  //   request
-  //   .put('/api/1/?data=kerfuffle')
-  //   .end((err, res) => {
-  //     if (err) return done(err);
-  //     assert.deepEqual(JSON.parse(res.text), { id: 1, data: 'kerfuffle' });
-  //     done();
-  //   });
-  // });
-  //
-  // it('is able to delete a record', function(done) {
-  //   request
-  //   .delete('/api/2')
-  //   .end((err, res) => {
-  //     if (err) return done(err);
-  //     // console.log(res.text);
-  //     assert.deepEqual(JSON.parse(res.text), [{'id':2, data:'barfoobaz'}]);
-  //     done();
-  //   });
-  // });
+  it('is able to get all records', function(done) {
+    request
+    .get('/')
+    .end((err, res) => {
+      if (err) return done(err);
+      // console.log(JSON.parse(res.text));
+      assert.deepEqual(JSON.parse(res.text), [ { id: 1, data: { data: 'foobarbaz' } }, { id: 2, data: { data:'barfoobaz' } } ]);
+      done();
+    });
+  });
+
+  it('is able to get a specific record', function(done) {
+    request
+    .get('/api/2')
+    .end((err, res) => {
+      if (err) return done(err);
+      // console.log((res));
+      assert.deepEqual(JSON.parse(res.text), { id: 2, data: { data:'barfoobaz' } });
+      done();
+    });
+  });
+
+  it('is able to update a record', function(done) {
+    request
+    .put('/api/1/')
+    .send({data: 'kerfuffle'})
+    .end((err, res) => {
+      if (err) return done(err);
+      assert.deepEqual(JSON.parse(res.text), { id: 1, data: {data: 'kerfuffle' }});
+      done();
+    });
+  });
+
+  it('is able to delete a record', function(done) {
+    request
+    .delete('/api/2')
+    .end((err, res) => {
+      if (err) return done(err);
+      // console.log(res.text);
+      assert.deepEqual(JSON.parse(res.text), [{'id':2, data: { data: 'barfoobaz'}}]);
+      done();
+    });
+  });
+
+  describe('handler unit tests', function() {
+    // it('bodyParser parses JSON', (done) => {
+    //   request
+    //   .send();
+    // });
+
+    it('errorhandler fires on junk requests', (done) => {
+      request
+      .get('/nonsense')
+      .end((err, res) => {
+        // console.log(res.body);
+        console.log(res.status);
+        // console.log('What Happened: ', err);
+        done();
+      });
+    });
+  });
 });
