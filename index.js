245e48049cb811e04096a47be4ea535de41cc133
Kate Sowles <katesowles@gmail.com>
2016-07-27 08:43:01 -0700

    progress but having trouble with middleware

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const server = require('./lib/server');
+const app = require('./lib/app');
+const http = require('http');
 
-const portNum = 3000;
+const portNum = 3000; // '= process.env.PORT || 3000;' ?
+
+const server = http.createServer(app);
 
 server.listen(portNum, () => {
-  console.log('server has started on ', server.address());
+  console.log(`to open server, load 'localhost:${portNum}' in a browser window`, server.address());
 });
