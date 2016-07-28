34d4f769843573e48c577a46a43e48f3faf4d689
Aaron Bini <bini.aaron@gmail.com>
2016-07-27 18:01:36 -0700

    movies endpoint routing and movies model complete

--- test/server-test.js
+++ test/server-test.js
@@ -3,9 +3,10 @@ const expect = chai.expect;
 const assert = chai.assert;
 const chaiHttp = require('chai-http');
 const server = require('../server');
-
 chai.use(chaiHttp);
-const req = chai.request(server);
+
+const connection = require('../lib/setup-mongoose');
+const app = require('../lib/app');
 
 describe('express routes and http verbs testing', () => {
   it('serves up homepage', (done) => {
@@ -18,57 +19,75 @@ describe('express routes and http verbs testing', () => {
       .catch(done);
   });
 
-  it('GETS all movies', (done) => {
-    req.get('/movies')
+  // before( done => {
+  //   const name = 'movies';
+  //   connection.on('open', () => {
+  //     connection.db
+  //     .listCollections({name})
+  //     .next((err, info) => {
+  //       if (!info) return done();
+  //       connection.db.dropCollection(name, done);
+  //     });
+  //   });
+  // });
+
+  const req = chai.request(server);
+
+  const testMovie = {title: 'A Few Good Men', year: '1990-06-06', category: 'Drama'};
+  const testMovie2 = {title: 'Test Movie', 'year': '1986-05-05', category: 'Adventure'};
+
+  it('POSTS a movie', (done) => {
+    req.post('/movies')
+      .send(testMovie)
       .then((res) => {
-        assert.equal('application/json; charset=utf-8', res.headers['content-type']);
-        expect(res).to.have.status(200);
-        expect(res.text).to.have.string('Items successfully retrieved.');
+        assert.ok(res.body._id);
+        testMovie.__v = 0;
+        testMovie._id = res.body._id;
+        console.log(testMovie._id);
+        testMovie.actors = [];
+        testMovie.awards = [];
+        testMovie.year = res.body.year;
         done();
       })
       .catch(done);
   });
 
   it('GETS one movie', (done) => {
-    req.get('/movies/0')
+    req.get(`/movies/${testMovie._id}`)
       .then((res) => {
-        assert.equal('application/json; charset=utf-8', res.headers['content-type']);
-        expect(res).to.have.status(200);
-        expect(res.text).to.have.string('Item successfully retrieved.');
+        assert.deepEqual(res.body, testMovie);
         done();
       })
       .catch(done);
   });
 
-  it('POSTS a movie', (done) => {
-    req.post('/movies')
-      .send({title: 'A Few Good Men', year: 1990})
+  it('GETS all after a post', (done) => {
+    req.get('/movies')
       .then((res) => {
-        assert.equal('application/json; charset=utf-8', res.headers['content-type']);
-        expect(res).to.have.status(200);
-        expect(res.text).to.have.string('Item successfully added.');
+        assert.deepEqual(res.body, [testMovie]);
         done();
       })
       .catch(done);
   });
 
   it('replaces one movie with PUT', (done) => {
-    req.put('/movies/1')
-      .send({title: 'Test Movie', 'year': 1986})
+    req.put(`/movies/${testMovie._id}`)
+      .send(testMovie2)
       .then((res) => {
-        assert.equal('application/json; charset=utf-8', res.headers['content-type']);
-        expect(res).to.have.status(200);
-        expect(res.text).to.have.string('Item successfully updated.');
+        testMovie2.__v = 0;
+        testMovie2._id = res.body._id;
+        testMovie2.actors = [];
+        testMovie2.awards = [];
+        testMovie2.year = res.body.year;
+        assert.deepEqual(res.body, testMovie2);
         done();
       }).catch(done);
   });
 
   it('DELETES a movie', (done) => {
-    req.delete('/movies/0')
+    req.delete(`/movies/${testMovie2._id}`)
       .then((res) => {
-        assert.equal('application/json; charset=utf-8', res.headers['content-type']);
-        expect(res).to.have.status(200);
-        expect(res.text).to.have.string('Item successfully deleted.');
+        assert.deepEqual(res.body, { ok: 1, n: 1 });
         done();
       })
       .catch(done);
