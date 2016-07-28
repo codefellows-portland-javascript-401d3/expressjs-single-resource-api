a37707a7d2ad990d876bd1658bacd4e4425d5a93
Aaron Bini <bini.aaron@gmail.com>
2016-07-28 13:38:23 -0700

    lab 3 completed with all routes and testing in place

--- test/server-test.js
+++ test/server-test.js
@@ -19,22 +19,10 @@ describe('express routes and http verbs testing', () => {
       .catch(done);
   });
 
-  // before( done => {
-  //   const name = 'movies';
-  //   connection.on('open', () => {
-  //     connection.db
-  //     .listCollections({name})
-  //     .next((err, info) => {
-  //       if (!info) return done();
-  //       connection.db.dropCollection(name, done);
-  //     });
-  //   });
-  // });
-
   const req = chai.request(server);
 
-  const testMovie = {title: 'A Few Good Men', year: '1990-06-06', category: 'Drama'};
-  const testMovie2 = {title: 'Test Movie', 'year': '1986-05-05', category: 'Adventure'};
+  const testMovie = {title: 'A Few Good Men', year: '1990-06-06', category: 'Drama', gross: 4000000};
+  const testMovie2 = {title: 'Test Movie', 'year': '1986-05-05', category: 'Adventure', gross: 3000000};
 
   it('POSTS a movie', (done) => {
     req.post('/movies')
@@ -43,10 +31,19 @@ describe('express routes and http verbs testing', () => {
         assert.ok(res.body._id);
         testMovie.__v = 0;
         testMovie._id = res.body._id;
-        console.log(testMovie._id);
         testMovie.actors = [];
         testMovie.awards = [];
+        testMovie.color = res.body.color;
         testMovie.year = res.body.year;
+        done();
+      })
+      .catch(done);
+  });
+
+  it('aggregates gross of movies in storage', (done) => {
+    req.get('/movies/gross')
+      .then(res => {
+        assert.deepEqual(res.body, {grossTotal: 4000000});
         done();
       })
       .catch(done);
@@ -78,6 +75,7 @@ describe('express routes and http verbs testing', () => {
         testMovie2._id = res.body._id;
         testMovie2.actors = [];
         testMovie2.awards = [];
+        testMovie2.color = res.body.color;
         testMovie2.year = res.body.year;
         assert.deepEqual(res.body, testMovie2);
         done();
@@ -107,6 +105,78 @@ describe('express routes and http verbs testing', () => {
       expect(res).to.have.status(404);
       done();
     });
+  });
+
+  const testActor = {name: 'Bill Murray', DOB: '1950-05-05', movies: ['test1', 'test2', 'test3']};
+  const testActor2 = {name: 'Meryl Streep', DOB: '1955-05-05', movies: []};
+
+  it('POSTS a movie', (done) => {
+    req.post('/actors')
+      .send(testActor)
+      .then((res) => {
+        assert.ok(res.body._id);
+        testActor.__v = 0;
+        testActor.DOB = res.body.DOB;
+        testActor._id = res.body._id;
+        testActor.movies = res.body.movies;
+        testActor.awards = [];
+        testActor.active = res.body.active;
+        testActor.age = res.body.age;
+        done();
+      })
+      .catch(done);
+  });
+
+  it('aggregates number of movies all actors have appeared in', (done) => {
+    req.get('/actors/totalMovies')
+      .then(res => {
+        assert.deepEqual(res.body, {totalMovies: 3});
+        done();
+      })
+      .catch(done);
+  });
+
+  it('GETS one actor', (done) => {
+    req.get(`/actors/${testActor._id}`)
+      .then((res) => {
+        assert.deepEqual(res.body, testActor);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('GETS all after a post', (done) => {
+    req.get('/actors')
+      .then((res) => {
+        assert.deepEqual(res.body, [testActor]);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('replaces one actor with PUT', (done) => {
+    req.put(`/actors/${testActor._id}`)
+      .send(testActor2)
+      .then((res) => {
+        testActor2.__v = 0;
+        testActor2._id = res.body._id;
+        testActor2.DOB = res.body.DOB;
+        testActor2.movies = [];
+        testActor2.awards = [];
+        testActor2.active = res.body.active;
+        testActor2.age = res.body.age;
+        assert.deepEqual(res.body, testActor2);
+        done();
+      }).catch(done);
+  });
+
+  it('DELETES an actor', (done) => {
+    req.delete(`/actors/${testActor2._id}`)
+      .then((res) => {
+        assert.deepEqual(res.body, { ok: 1, n: 1 });
+        done();
+      })
+      .catch(done);
   });
 
 });
