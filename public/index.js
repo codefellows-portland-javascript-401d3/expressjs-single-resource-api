f6aad3aa36a7835f5f6817229be11f97f5e4acbc
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-08-03 16:48:43 -0700

    signup and login working

--- public/index.js
+++ public/index.js
@@ -1,32 +1,56 @@
 const seriesToHtml = Handlebars.compile($('#series-template').html());
 
-$.ajax('/api/series', {
-  success: data => {
-    const series = { data };
-    const html = seriesToHtml(series);
-    $('#series-list').append(html);
-  },
-  error: () => $('#notification-bar').text('Error occurred getting series list')
-});
+function loadSeries() {
+  $.ajax('/api/series', {
+    success: data => {
+      const series = { data };
+      const html = seriesToHtml(series);
+      $('#series-list').empty().append(html);
+    },
+    error: () => $('#notification-bar').text('Error occurred getting series list')
+  });
+}
+
+loadSeries();
 
 const episodesToHtml = Handlebars.compile($('#episode-template').html());
 
-$.ajax('/api/episodes', {
-  success: data => {
-    const episode = { data };
-    const html = episodesToHtml(episode);
-    $('#episode-list').append(html);
-  },
-  error: () => $('#notification-bar').text('Error occurred getting episode list')
-});
+function loadEpisodes() {
+  $.ajax('/api/episodes', {
+    success: data => {
+      const episode = { data };
+      const html = episodesToHtml(episode);
+      $('#episode-list').empty().append(html);
+    },
+    error: () => $('#notification-bar').text('Error occurred getting episode list')
+  });
+}
 
-$('#episode-list').on('click', '.delete', function() {
+loadEpisodes();
+
+const usersToHtml = Handlebars.compile($('#userlist-template').html());
+
+function loadUsers() {
+  $.ajax('/api/users', {
+    success: data => {
+      const episode = { data };
+      const html = usersToHtml(episode);
+      $('#user-list').empty().append(html);
+    },
+    error: () => $('#notification-bar').text('Error occurred getting episode list')
+  });
+}
+
+loadUsers();
+
+$('#user-list,#episode-list,#series-list').on('click', '.delete', function() {
   const selected = $(this).data();
   $.ajax(`/api/${selected.type}/${selected.id}`, {
     type: 'DELETE',
     success: data => {
-      window.location('/');
-      $('#notification-bar').text('Deleted:', data.name || data.title);
+      selected.type === 'series' ? loadSeries() : selected.type === 'episodes' ? loadEpisodes() : loadUsers();
+      $('#notification-bar').text('Deleted: ' + (data.name || data.title || data.username));
+      setTimeout( () => $('#notification-bar').empty() , 10000);
     },
     error: () => $('#notification-bar').text('Error occurred deleting', selected)
   });
