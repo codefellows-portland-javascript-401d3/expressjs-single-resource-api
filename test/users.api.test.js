9a63d82f0b5feece60c4470769fc5c91dc2f21be
Arielle Foldoe <afoldoe@gmail.com>
2016-08-03 14:41:35 -0700

    finished lab 5, base assignment

--- test/users.api.test.js
+++ test/users.api.test.js
@@ -17,15 +17,43 @@ describe(`users`, () => {
   });
 
   const request = chai.request(app);
-
-  const sample = "name: Arielle";
+  let sample1 = {name: `Marcus`, password: `password`};
   let sample2 = {};
+  let token = /eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9/;
+  let returnedToken;
+
+  it(`tests signup for a user`, done => {
+    request
+      .post(`/auth/signup`)
+      .send(sample1)
+      .then(res => {
+        returnedToken = res.body;
+        assert.isObject(res.body);
+        assert.match(returnedToken.token, token);
+        done();
+      })
+      .catch(done);
+  });
+
+  it(`tests signing in for a user`, done => {
+    request
+      .post(`/auth/signin`)
+      .send({name: `Marcus`, password: `password`})
+      .then(res => {
+        returnedToken = res.body;
+        assert.isObject(res.body);
+        assert.match(returnedToken.token, token);
+        done();
+      })
+      .catch(done);
+  });
 
   it(`gets all`, done => {
     request
       .get(`/users`)
+      .set(`token`, returnedToken.token)
       .then(res => {
-        assert.deepEqual(res.body, []);
+        assert.deepEqual(res.body.length, 1);
         done();
       })
       .catch(done);
@@ -35,7 +63,8 @@ describe(`users`, () => {
   it(`adds user to the db`, done => {
     request
       .post(`/users`)
-      .send({name: `Arielle`})
+      .set(`token`, returnedToken.token)
+      .send({name: `Arielle`, password: `password`})
       .then(res => {
         const user = res.body;
         assert.equal(res.status, 200);
@@ -49,6 +78,7 @@ describe(`users`, () => {
   it(`gets a user by id`, done => {
     request 
       .get(`/users/${sample2._id}`)
+      .set(`token`, returnedToken.token)
       .then(res => {
         let user = res.body;
         assert.deepEqual(user._id, sample2._id);
@@ -60,6 +90,7 @@ describe(`users`, () => {
   it(`changes a user's name`, done => {
     request
       .put(`/users/${sample2._id}`)
+      .set(`token`, returnedToken.token)
       .send(`name: Brian`)
       .then(res => {
         let user = res.body;
@@ -73,6 +104,7 @@ describe(`users`, () => {
   it(`delets a user by id`, done => {
     request
       .del(`/users/${sample2._id}`)
+      .set(`token`, returnedToken.token)
       .then(res => {
         let user = res.body;
         assert.equal(sample2._id, user._id);
